5cc6e4e74df60dc99489bbf19a2d37400b5e3e82
Saikat <bdappdeveloper@gmail.com>
2019-05-03 01:50:25 +0600

    Project Setup with firebase

---
+++ app/src/main/java/com/project/axislab/oneummah/activity/MosqueListActivity.java
@@ -0,0 +1,15 @@
+package com.project.axislab.oneummah.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.project.axislab.oneummah.R;
+
+public class MosqueListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mosque_list);
+    }
+}
